c7c27ac930e1243d1dc0115e93a77e4b99dc8a11
Jeison Higuita <jeisonhs93@gmail.com>
2020-08-03 21:27:27 +0200

    change index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,50 @@
+<script>
+  import { ops, capitalize } from "./helpers";
+  let height = 100;
+  let width = 100;
+  let doors = 2;
+  let system = "520";
+
+  const availableSystems = ["520", "744"];
+  const availableDoors = [2, 3, 4];
+
+  const calc = (s, w, h) => ops[s](w, h);
+
+  $: calculatedSystem = calc(system, width, height);
+  $: measures = Object.keys(calculatedSystem);
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  .input {
+    display: block;
+    max-width: 25rem;
+    margin-bottom: 1rem;
+    width: 100%;
+    font-size: 1.5rem;
+  }
+  .box {
+    display: flex;
+    padding: 1rem;
+    flex-direction: column;
+    border-radius: 8px;
+    background: aliceblue;
+    border: 1px solid #c5def4;
+  }
 </style>
 
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
+<input class="input" bind:value={height} placeholder={'Alto'} type="number" />
+<input class="input" bind:value={width} placeholder={'Ancho'} type="number" />
+<select class="input" bind:value={system}>
+  {#each availableSystems as currentSystem}
+    <option value={currentSystem}>{`Sistema ${currentSystem}`}</option>
+  {/each}
+</select>
+<div class="box">
+  <h2>
+    Las medidas para una ventana {width}x{height}cms del sistema {system} son:
+  </h2>
+  {#each measures as measure}
+    <p>{capitalize(measure)}: {calculatedSystem[measure]}cm</p>
+  {/each}
 
-<h1>Great success!</h1>
-
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+</div>
